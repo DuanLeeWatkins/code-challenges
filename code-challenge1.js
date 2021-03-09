3182a19894c5e68f33243dc3e95a4bc3ba357f30
Duan Lee-Watkins <leeduan19@gmail.com>
2021-03-09 12:39:12 -0500

    update code-challenge1

--- code-challenge1.js
+++ code-challenge1.js
@@ -4,9 +4,3 @@ function array(arr) {
   return arr.split(",").slice(1, -1).join(" ") || null;
 }
 
-Test.describe("Tests", function () {
-  Test.assertEquals(array(""), null);
-  Test.assertEquals(array("1"), null);
-  Test.assertEquals(array("1, 3"), null);
-  Test.assertEquals(array("1,2,3"), "2");
-});
